33be443197c06ba73237b0614f70bcdd393ef6cd
DEEPIGA1608 <113665160+DEEPIGA1608@users.noreply.github.com>
2024-02-09 12:20:21 +0530

    Add files via upload

---
+++ BACKEND/Backend/Day11/mobilerecharge/src/test/java/com/recharge/mobilerecharge/MobilerechargeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.recharge.mobilerecharge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilerechargeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	
+}
